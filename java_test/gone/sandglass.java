a6dd5e6bd96e3ce389843d6d0390e461df7685a6
gone <dbcksrhs30@yahoo.co.kr>
2021-03-14 03:48:33 +0900

    modify sandglass

--- java_test/gone/sandglass.java
+++ java_test/gone/sandglass.java
@@ -1,7 +1,35 @@
 package java_test.gone;
 
+/* 모래 시계 찍기 */ 
 public class sandglass {
     public static void main(String[] args) {
-        // 모레시계 찍기
+        // 첫번 째 가로
+        for(int i=1;i<=6;i++){
+            // 첫번 째 세로 5개
+            for(int j=i;j<=6;j++){
+                if(j==i){
+                    System.out.print("$");
+                }
+                System.out.print("*");
+                if(j==6){
+                    System.out.print("$");
+                }
+            }
+            System.out.println();
+        }
+        // 두번 째 가로
+        for(int k = 1;k<=5;k++){
+            // 두번 째 세로 5개
+            for(int n = 0;n<=k;n++){
+                if(n==k){
+                    //System.out.print("$");
+                }
+                System.out.print("*");
+                if(n==k){
+                    System.out.print("&");
+                }
+            }
+            System.out.println();
+        }
     }
 }
