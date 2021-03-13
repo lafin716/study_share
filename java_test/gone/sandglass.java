871cd4a5ec42e05a8cf7424e0b4a971913629bf4
gone <dbcksrhs30@yahoo.co.kr>
2021-03-14 01:39:53 +0900

    추가 #00000 스터디파일 추가

---
+++ java_test/gone/sandglass.java
@@ -0,0 +1,5 @@
+public class sandglass {
+    public static void main(String[] args) {
+        // 모레시계 찍기
+    }
+}
---
+++ java_test/gone/starPrint.java
@@ -0,0 +1,15 @@
+public class starPrint {
+    public static void main(String[] args) {
+        /*
+         * Class Name: StarPrint
+         * Main Method Create
+         * Useing for / width: 10개 / Hegiht: 7 pirnt 
+         */
+        for(int i = 1;i<=10;i++){
+            for(int j = 1;j<=7;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
