6249420adef919efb9be9c699e1aa610a03fe8c8
gonegone <dbcksrhs30@yahoo.co.kr>
2021-03-24 22:44:58 +0900

    add lottery rule

--- java/gone/lottery_wheel.java
+++ java/gone/lottery_wheel.java
@@ -1,5 +1,11 @@
 package java.gone;
 
 public class lottery_wheel {
-    
+    public static void main(String[] args) {
+        /* 로또 규칙
+         * 1~45까지의 숫자만 나올 수 있다.
+         * 6개만 당첨 숫자가 될 수 있다.
+         * 숫자는 매번 랜덤으로 출력된다.
+         */
+    }
 }
